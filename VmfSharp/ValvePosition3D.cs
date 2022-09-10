ad46e117343e13077e2854a3224e9a6ddb0db435
Simon <h45h74x@gmail.com>
2022-09-10 15:47:11 +0200

    Set concealed block faces to 'nodraw'

---
+++ VmfSharp/ValvePosition3D.cs
@@ -0,0 +1,14 @@
+﻿namespace VmfSharp
+{
+  public enum ValvePosition3D
+  {
+    West, // X-
+    East, // X+
+    
+    South, // Z+
+    North, // Z-
+    
+    Top, // Y+
+    Bottom, // Y-
+  }
+}
